44034da0d65e3afee9187d5bc6a268cba964c83b
zhaoyadan <forwellzia@gmail.com>
2019-08-04 17:40:28 +0800

    vue项目实例

--- front-end/node/start/btoa.js
+++ front-end/node/start/btoa.js
@@ -1,13 +1,6 @@
-const btoa = require('btoa');
-const http = require('http');
-
-var auth1 = btoa("{\"alg\":\"none\"");
-
-var auth2 = btoa("\"email\":\"callbuisse@gmail.com\",\"iss\":\"AfUf3_kuT-nNLjG1zXEaBrGNqXlwqnyPvmZeyVaOuBo5K-sHJOwBbhbe5ycz4p6SDPFU4K4pIkiW5z6C\"");
-
-console.log(auth1);
-console.log(auth2);
-
-var auth_assertion_header = auth1 + "." + auth2 + ".";
+var btoa = require('btoa');
+var auth_1 = btoa("{\"alg\":\"none\"}");
+var auth_2 = btoa("{\"email\":callbuisse@gmail.com,\"iss\":AZ6OsG3kJ9g3kWtVJcm7ZU5H570_t8_-yqJzhRmPY-t38Gcz_ntAx5MrfgpVU0foEDCRczj9ggQ_q4dz}");
+var auth_assertion_header = auth_1 + "." + auth_2 + ".";
 
 console.log(auth_assertion_header);
