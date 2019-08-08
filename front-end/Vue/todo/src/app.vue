a02b52f1eafb37efaac49a5285841ac386891fca
ornary <forwelll@163.com>
2019-08-08 08:13:03 +0800

    todo

--- front-end/Vue/todo/src/app.vue
+++ front-end/Vue/todo/src/app.vue
@@ -1,32 +1,39 @@
 <template>
-	<div id="app">
-		<div id="cover"></div>
-	</div>
+<div id="app">
+    <div id="cover"></div>
+	<Header></Header>
+	<todo></todo>
+	<Footer></Footer>
+</div>
 </template>
 
 <script>
-	export default {
+import Header from './todo/header.vue';
+import Footer from './todo/footer.jsx';
+import Todo from './todo/todo.vue';
 
+export default {
+	components: {
+		Header,
+		Footer,
+		Todo
 	}
+}
 </script>
 
 <style lang="stylus" scoped>
-#app{
-	position absolute
-	left 0
-	right 0
-	top 0
-	bottom 0
-}
-
-#cover{
-	position absolute
-	top 0
-	left 0
+#app 
+	position absolute 
+	left 0 
+	right 0 
+	top 0 
+	bottom 0 
+#cover 
+	position absolute 
+	top 0 
+	left 0 
 	right 0
 	bottom 0
-	background-color #999
-	opacity .9
+	background-color #999 opacity .9 
 	z-index -1
-}
 </style>
